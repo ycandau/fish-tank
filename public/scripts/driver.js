cdb37d53cad300b2199424611a4fa42049f4a1ee
Yves Candau <ycandau@gmail.com>
2021-07-02 06:11:52 +0000

    Added BiteFish class

--- public/scripts/driver.js
+++ public/scripts/driver.js
@@ -1,13 +1,8 @@
 $(() => {
-
-  window.fishtank = new Fishtank('fishtank');             // making it global to make debugging 1% easier.  shhhh.
-  window.fishtank.registerSpecies(SwitchFish, GoFish);
+  window.fishtank = new Fishtank("fishtank"); // making it global to make debugging 1% easier.  shhhh.
+  window.fishtank.registerSpecies(SwitchFish, GoFish, BiteFish);
   var starter = new Starter({
-    tank: window.fishtank,     // look, it's Dependency Injection!  Pretend you care!
-    position: new Vector(0, 0)
+    tank: window.fishtank, // look, it's Dependency Injection!  Pretend you care!
+    position: new Vector(0, 0),
   });
-
 });
-
-
-
---
+++ public/scripts/entities/bite_fish.js
@@ -0,0 +1,6 @@
+class BiteFish extends Fish {
+  constructor(options) {
+    super(options);
+    this.imageUri = "/images/bite_fish.gif";
+  }
+}
